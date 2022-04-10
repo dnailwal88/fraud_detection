de702f3f74aa68c8a273fea73d71aa2b56e294e3
dnailwal88 <deepaknailwal88@gmail.com>
2022-04-10 19:35:46 +0530

    model2 will be used as it is the best model

--- ml_model.py
+++ ml_model.py
@@ -1,10 +1,12 @@
 print("ML related code here")
 
 def model1():
-    print("ML model 1")
+    print("ML model1")
 
 def model2():
-    print("ML model 2")
+    print("ML model2")
 
 def model3():
-    print("ML model 3")
+    print("ML model3")
+
+print("model 2 is performing the best")
